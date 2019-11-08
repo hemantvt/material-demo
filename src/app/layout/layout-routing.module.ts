cbe708211c9bf0e3400a8935b61b5498c36627fa
“hemantvt” <hemantkumarshakyawal@gmail.com>
2019-11-08 14:12:34 +0530

    update npm

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -10,13 +10,8 @@ const routes: Routes = [
          { path: '', redirectTo: 'dashboard', pathMatch: 'prefix' },
              { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
              { path: 'registration', loadChildren: () => import('./registration/registration.module').then(m => m.RegistrationModule) },
-          { path: 'form-controls', loadChildren: () => import('./form-controls/formcontrol.module').then(m => m.FormcontrolModule) },
-            // { path: 'tables', loadChildren: () => import('./tables/tables.module').then(m => m.TablesModule) },
-            // { path: 'forms', loadChildren: () => import('./form/form.module').then(m => m.FormModule) },
-            // { path: 'bs-element', loadChildren: () => import('./bs-element/bs-element.module').then(m => m.BsElementModule) },
-            // { path: 'grid', loadChildren: () => import('./grid/grid.module').then(m => m.GridModule) },
-            // { path: 'components', loadChildren: () => import('./bs-component/bs-component.module').then(m => m.BsComponentModule) },
-            // { path: 'blank-page', loadChildren: () => import('./blank-page/blank-page.module').then(m => m.BlankPageModule) }
+          { path: 'form-controls', loadChildren: () => import('./form-controls/formcontrol.module').then(m => m.FormcontrolModule) }
+
         ]
     }
 ];
